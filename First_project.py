570a96825909abae614ce582b6817aa107105bd4
LZ <126319831+LZf1r3@users.noreply.github.com>
2023-10-25 11:53:16 -0700

    Update First_project.py

--- First_project.py
+++ First_project.py
@@ -1,31 +1,67 @@
-import pygame # Importing the python gaming library
-from sys import exit # Import exit from the system library
+import pygame  # Importing the python gaming library
+from sys import exit  # Import exit from the system library
 
-pygame.init() # Initializing pygame
-screen = pygame.display.set_mode((800,400)) # Defining the size of the screen/window
-pygame.display.set_caption("LZ's First Game") # Defining the name on the window
-clock = pygame.time.Clock() # Creating an object for frameratelimit
-test_font = pygame.font.Font('UltimatePygameIntro-main/font/Pixeltype.ttf',50) # pygame.font.Font(font type, font size )
-snail_surface = pygame.image.load('UltimatePygameIntro-main/graphics/snail/snail1.png').convert_alpha() # Importing snail image <convert_alpha means that when converting the image to a easier picture it will keep the original values such as png
-sky_surface = pygame.image.load('UltimatePygameIntro-main\graphics\Sky.png').convert() # Importing sky picture for background
-ground_surface = pygame.image.load('UltimatePygameIntro-main\graphics\ground.png').convert() # Importing ground picture for the ground
-text_surface = test_font.render("My Game",False,"Black") # teste_font.render(text, AA, color) <AA means anti-aliasing, which is to smooth the edges of the text>
-snail_x_position = 800 # Defining the starting position of the snail
+pygame.init()  # Initializing pygame
+# Defining the size of the screen/window
+screen = pygame.display.set_mode((800, 400))
+# Defining the name on the window
+pygame.display.set_caption("LZ's First Game")
+clock = pygame.time.Clock()  # Creating an object for frameratelimit
+# pygame.font.Font(font type, font size )
+test_font = pygame.font.Font(
+    'UltimatePygameIntro-main/font/Pixeltype.ttf', 50)
+# Importing snail image <convert_alpha means that when converting the image to a easier picture it will keep the original values such as png
+snail_surface = pygame.image.load(
+    'UltimatePygameIntro-main/graphics/snail/snail1.png').convert_alpha()
+snail_rectangle = snail_surface.get_rect(midbottom=(700, 300))
 
-player_surface = pygame.image.load("UltimatePygameIntro-main/graphics/player/player_walk_1.png").convert_alpha()
+# Importing sky picture for background
+sky_surface = pygame.image.load(
+    'UltimatePygameIntro-main/graphics/Sky.png').convert()
+# Importing ground picture for the ground
+ground_surface = pygame.image.load(
+    'UltimatePygameIntro-main/graphics/ground.png').convert()
+# teste_font.render(text, AA, color) <AA means anti-aliasing, which is to smooth the edges of the text>
+text_surface = test_font.render("My Game", False, "Black")
 
-while True: # Loop to keep the window's appearance 
-    for event in pygame.event.get(): # Loop for checking input from the user in case they want to quit the game
-        if event.type == pygame.QUIT: # Checking event
-            pygame.quit() # Quiting pygame
-            exit() # Ending every functioning code
-    
-    screen.blit(sky_surface,(0,0)) # Screen.blit(surface escolhida, (posicao))
-    screen.blit(ground_surface,(0,300)) # Defining the position of the ground
-    screen.blit(text_surface,(300,50)) # Defining the position of the text
-    snail_x_position -=0.5 # Creating the animation on the snail sprite
-    screen.blit(snail_surface,(snail_x_position,250)) # Creating the 
-    if snail_x_position <= -100:snail_x_position = 800
-    screen.blit(player_surface,(80,200))
-    pygame.display.update() # Updating window
-    clock.tick(200) # limiting framerate to 120fps
+player_surface = pygame.image.load(
+    "UltimatePygameIntro-main/graphics/player/player_walk_1.png").convert_alpha()
+# player_surface.ger_rect(topleft = (x,y))
+player_rectangle = player_surface.get_rect(midbottom=(80, 300))
+
+while True:  # Loop to keep the window's appearance
+    for event in pygame.event.get():  # Loop for checking input from the user in case they want to quit the game
+        if event.type == pygame.QUIT:  # Checking event
+            pygame.quit()  # Quiting pygame
+            exit()  # Ending every functioning code
+    #    if event.type == pygame.MOUSEMOTION:
+    #        print(event.pos)
+    #    if event.type == pygame.MOUSEBUTTONDOWN:
+    #        print("mouse down")
+    #    if event.type == pygame.MOUSEBUTTONUP:
+    #        print("mouse up")
+    # Screen.blit(surface escolhida, (posicao))
+        if event.type == pygame.MOUSEMOTION:
+            if player_rectangle.collidepoint(event.pos):
+                print("collision")
+    screen.blit(sky_surface, (0, 0))
+    # Defining the position of the ground
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))  # Defining the position of the text
+
+    snail_rectangle.x -= 1
+    if snail_rectangle.x <= -100:
+        snail_rectangle.x = 800
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_rectangle)
+    if snail_rectangle.colliderect(player_rectangle):print("Dead"),exit()
+
+    # if player_rectangle.colliderect(snail_rectangle):
+    #    print("Collision")
+
+    # mouse_position = pygame.mouse.get_pos()
+    # if player_rectangle.collidepoint((mouse_position)):
+    #    print(pygame.mouse.get_pressed())
+
+    pygame.display.update()  # Updating window
+    clock.tick(120)  # limiting framerate to 120fps
